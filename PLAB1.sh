107d0a0a059e9b8dee3184c7584220957448871a
ferran <1751659@uab.cat>
2024-10-14 18:02:54 +0200

    Commit 5

--- PLAB1.sh
+++ PLAB1.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#Execusió del primer awk
+awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
+
+#Pregunta 1
+cut -d',' -f12,16 --complement supervivents.csv >> sortida1.csv 
+
+#Pregunta 2
+cut -d',' -f 14 sortida1.csv | grep True | wc -l
+awk -F, '$14 != "True" {print $0}' sortida1.csv > sortida2.csv
+
+#Pregunta 3
 awk -F, 'BEGIN { OFS = "," } 
 NR == 1 { print $0, "Ranking_Views"; next }
 {
@@ -7,4 +18,24 @@ NR == 1 { print $0, "Ranking_Views"; next }
     else if ($8 > 1000000 && $8 <= 10000000) ranking="Excel·lent";
     else if ($8 > 10000000) ranking="Estrella";
     print $0, ranking;
-}' sortida.csv > sortida3.csv
+}' sortida2.csv > sortida3.csv
+
+#Pregunta 4
+n=1
+while read lin; do
+    if [ $n -eq 1 ]; then
+        echo "$lin,Rlikes,Rdislikes"
+        n=$((n+1))
+    else
+        views=$(echo $lin | cut -d',' -f8)
+        likes=$(echo $lin | cut -d',' -f9)
+        dislikes=$(echo $lin | cut -d',' -f10)
+        percent_likes=$(( ($likes*100)/$views ))
+        percent_dislikes=$(( ($dislikes*100)/$views )) 
+        echo "$lin,$percent_likes,$percent_dislikes"
+    fi
+done < sortida3.csv > sortida4.csv
+
+
+
+
